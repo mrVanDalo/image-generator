4ea3463db9bfde900df98d8e9051cd0654bfb84f
Ingolf Wagner <contact@ingolf-wagner.de>
2020-02-15 01:54:50 +0800

    update default.nix

--- default.nix
+++ default.nix
@@ -1,18 +1,18 @@
-{ rustPlatform, fetchFromGitHub, stdenv, ... }:
+{ rustPlatform, fetchgit, stdenv, cairo, ... }:
 
 rustPlatform.buildRustPackage rec {
   name = "image-geneartor-${version}";
   version = "2.0.0";
-  src = ./.;
-  #src = fetchFromGitHub {
-  #  owner = "mrVanDalo";
-  #  repo = "taskwarrior-hooks";
-  #  rev = "${version}";
-  #  sha256 = "1mj0k6ykac332667315kqrvg37j8r8078g48nafv7ini6lw8djas";
-  #};
-
-  cargoSha256 = "1ijnh2ank9slmfglw4yhnycl11x26m94m2hiq3hcasmbs6c39zj5";
+  # src = ./.;
+  src = fetchgit {
+    url = "https://git.ingolf-wagner.de/palo/image-generator2";
+    rev = "c9ef48b1bd73a819a5fb86095774472f35d7b576";
+    sha256 = "0pdhw2calqxczdrz66wyvydd5d6m2fx7xm64z1l25nazdad930p5";
+  };
+  cargoSha256 = "07pwds279qc54g5fza805ah2m8jhrdzng7vb1bi24a9ra9ci8s29";
   verifyCargoDeps = true;
+
+  buildInputs = [ cairo ];
 
   meta = with stdenv.lib; {
     description =
