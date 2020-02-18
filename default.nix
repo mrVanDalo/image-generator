ddd31bb322fec53cfbd7466d19baa0a66653022c
Ingolf Wagner <contact@ingolf-wagner.de>
2020-02-18 13:23:59 +0800

    update Cargo.toml

--- default.nix
+++ default.nix
@@ -2,12 +2,12 @@
 
 rustPlatform.buildRustPackage rec {
   name = "image-geneartor-${version}";
-  version = "2.0.0";
+  version = "3.0.0";
   # src = ./.;
-  src = fetchgit {
-    url = "https://git.ingolf-wagner.de/palo/image-generator2";
-    rev = "78cdc84a90cd818561a70bf1868ed3875d1e2204";
-    sha256 = "1r0splgsfc2sf014s71r91272icdqgfkxsp62k2jav0spwhi7sqy";
+  src = fetchgit{
+    url = "https://github.com/mrVanDalo/image-generator";
+    rev = "f8c1bf958aeea1808df6baea62eeb2949aa5fe65";
+    sha256 = "1swa2i57sdag3zapzx3m9mdarci0xfjczr611320zampw505ai09";
   };
 
   cargoSha256 = "07pwds279qc54g5fza805ah2m8jhrdzng7vb1bi24a9ra9ci8s29";
@@ -18,7 +18,7 @@ rustPlatform.buildRustPackage rec {
   meta = with stdenv.lib; {
     description =
       "An image generator unsing entropy and a JSON as configuration.";
-    homepage = "https://git.ingolf-wagner.de/palo/image-generator2";
+    homepage = "https://github.com/mrVanDalo/image-generator";
     license = licenses.gplv3;
     maintainers = [ maintainers.mrVanDalo ];
     platforms = platforms.all;
