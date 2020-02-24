7f5fd75ffa0f4ebd531f7f01b51bbec26ff000f4
Ingolf Wagner <contact@ingolf-wagner.de>
2020-02-24 17:29:06 +0800

    add some documentation

--- src/rendable.rs
+++ src/rendable.rs
@@ -1,10 +1,17 @@
+//! provides a trait with a render function.
+//! also provides some functions related
+//! to rendering.
+
 use crate::objects::Color;
 use crate::structure::ImageContext;
 use cairo::Context;
 
+/// objects that can be rendered.
 pub trait Rendable {
+    /// render an object
     fn render(&self, context: &Context, image_context: &ImageContext, depth: i32);
 
+    /// configure the color to draw with
     fn configure_color(&self, color: &Color, context: &Context, image_context: &ImageContext) {
         match &color {
             Color::Fill => {
@@ -26,6 +33,7 @@ pub trait Rendable {
         }
     }
 
+    /// a stroke function that preserves the line with
     #[inline(always)]
     fn stroke_and_preserve_line_width(&self, context: &Context) {
         // todo: just add this function to context via trait
